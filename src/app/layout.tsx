54c995e951eb439b28d283adb6e39bb6d5caabd3
Anish-1144 <dev.anishmourya27@gmail.com>
2025-04-04 17:15:59 +0530

    Add Facebook Pixel tracking script and optimize image loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script"; // Import Script for optimized loading
+import Image from "next/image"; // Import Image component
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,6 +26,38 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Meta Pixel Script */}
+        <Script
+          id="facebook-pixel" // Added id attribute to fix the error
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1402398147878228');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          {/* Replaced img with next/image and added alt attribute */}
+          <Image
+            height={1}
+            width={1}
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1402398147878228&ev=PageView&noscript=1"
+            alt="Facebook Pixel"
+            unoptimized // Use unoptimized for tracking pixels
+          />
+        </noscript>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
